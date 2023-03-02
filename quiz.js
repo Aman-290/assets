28c453d504b999342ed84ddd081ca9e711e91ea9
Aman J Sonal <79847765+Aman-290@users.noreply.github.com>
2023-03-02 17:37:47 +0530

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,279 @@
+export const questions = [
+{
+"question": "What is the name of the school that Harry Potter attends?",
+"choices": ["Hogwarts School of Witchcraft and Wizardry", "Ilvermorny School of Witchcraft and Wizardry", "Beauxbatons Academy of Magic", "Durmstrang Institute"],
+"answer": "Hogwarts School of Witchcraft and Wizardry"
+},
+
+{
+"question": "Who is the headmaster of Hogwarts School of Witchcraft and Wizardry?",
+"choices": ["Albus Dumbledore", "Severus Snape", "Minerva McGonagall", "Gilderoy Lockhart"],
+"answer": "Albus Dumbledore"
+},
+
+{
+"question": "What is the name of Harry Potter's owl?",
+"choices": ["Hedwig", "Errol", "Pigwidgeon", "Grip Hook"],
+"answer": "Hedwig"
+},
+
+{
+"question": "What is the name of the evil wizard who killed Harry's parents?",
+"choices": ["Voldemort", "Lucius Malfoy", "Bellatrix Lestrange", "Fenrir Greyback"],
+"answer": "Voldemort"
+},
+
+{
+"question": "What is the name of the sport played on broomsticks in the Harry Potter series?",
+"choices": ["Quidditch", "Bludgerball", "Broomstick Polo", "Flying Soccer"],
+"answer": "Quidditch"
+},
+
+{
+"question": "What is the name of the school newspaper in the Harry Potter series?",
+"choices": ["The Daily Prophet", "The Quibbler", "The Hogwarts Herald", "The Wizarding Weekly"],
+"answer": "The Daily Prophet"
+},
+
+{
+"question": "What is the name of the prison guarded by dementors in the Harry Potter series?",
+"choices": ["Azkaban", "Alcatraz", "Devil's Island", "Sing Sing"],
+"answer": "Azkaban"
+},
+
+{
+"question": "What is the name of the potion that enables a person to transform into someone else?",
+"choices": ["Polyjuice Potion", "Veritaserum", "Amortentia", "Felix Felicis"],
+"answer": "Polyjuice Potion"
+},
+
+{
+"question": "What is the name of the three-headed dog that guards the Philosopher's Stone?",
+"choices": ["Fluffy", "Fang", "Norbert", "Aragog"],
+"answer": "Fluffy"
+},
+
+{
+"question": "What is the name of the device used to travel back in time in the Harry Potter series?",
+"choices": ["Time-Turner", "Chrono-Crystal", "Temporal Displacer", "Quantum Leap Device"],
+"answer": "Time-Turner"
+},
+
+{
+"question": "What is the name of the house elf who belonged to the Black family and later to Harry Potter?",
+"choices": ["Kreacher", "Dobby", "Winky", "Hokey"],
+"answer": "Kreacher"
+},
+
+{
+"question": "Who is the potions professor at Hogwarts School of Witchcraft and Wizardry?",
+"choices": ["Severus Snape", "Horace Slughorn", "Pomona Sprout", "Filius Flitwick"],
+"answer": "Severus Snape"
+},
+
+{
+"question": "What is the name of the house ghost of Ravenclaw?",
+"choices": ["The Grey Lady", "The Bloody Baron", "Nearly Headless Nick", "The Fat Friar"],
+"answer": "The Grey Lady"
+},
+
+{
+"question": "What is the name of the ghost who haunts the girl's bathroom on the second floor of Hogwarts?",
+"choices": ["Moaning Myrtle", "Whispering Wendy", "Screaming Sarah", "Crying Caroline"],
+"answer": "Moaning Myrtle"
+},
+
+{
+"question": "What is the name of the spell used to disarm an opponent?",
+"choices": ["Expelliarmus", "Stupefy", "Avada Kedavra", "Crucio"],
+"answer": "Expelliarmus"
+},
+
+{
+"question": "What is the name of the magical creature that can only be seen by those who have witnessed death?",
+"choices": ["Thestrals", "Hippogriffs", "Blast-Ended Skrewts", "Nifflers"],
+"answer": "Thestrals"
+},
+
+{
+"question": "What is the name of the gamekeeper at Hogwarts School of Witchcraft and Wizardry?",
+"choices": ["Rubeus Hagrid", "Argus Filch", "Wilhelmina Grubbly-Plank", "Irma Pince"],
+"answer": "Rubeus Hagrid"
+},
+
+{
+"question": "What is the name of the wizarding bank in the Harry Potter series?",
+"choices": ["Gringotts", "Vault of Merlin", "Dragon's Hoard", "Goblin's Treasure"],
+"answer": "Gringotts"
+},
+
+{
+"question": "What is the name of the magical plant that can be used to restore a person's health and strength?",
+"choices": ["Mandrake", "Devil's Snare", "Venomous Tentacula", "Gillyweed"],
+"answer": "Mandrake"
+},
+
+{
+"question": "What is the name of the spell used to create a protective shield?",
+"choices": ["Protego", "Expecto Patronum", "Levicorpus", "Impedimenta"],
+"answer": "Protego"
+},
+
+{
+"question": "What is the name of the wizarding prison located in the Ministry of Magic?",
+"choices": ["Azkaban", "Nurmengard", "St. Mungo's", "The Burrow"],
+"answer": "Azkaban"
+},
+
+{
+"question": "What is the name of the organization founded by Albus Dumbledore to fight against Voldemort and his followers?",
+"choices": ["The Order of the Phoenix", "The Death Eaters", "The Knights of Walpurgis", "The Dark Brotherhood"],
+"answer": "The Order of the Phoenix"
+},
+
+{
+"question": "What is the name of the potion that induces luck?",
+"choices": ["Liquid Luck", "Felix Felicis", "Fortune Potion", "Chance Elixir"],
+"answer": "Felix Felicis"
+},
+
+{
+"question": "What is the name of the house-elf who was freed by Harry Potter in the second book?",
+"choices": ["Dobby", "Winky", "Kreacher", "Hokey"],
+"answer": "Dobby"
+},
+
+{
+"question": "What is the name of the device that allows wizards to communicate with each other from long distances?",
+"choices": ["The Floo Network", "The Portkey", "The Vanishing Cabinet", "The Hogwarts Express"],
+"answer": "The Floo Network"
+},
+
+{
+"question": "What is the name of the room in Hogwarts that appears only when someone is in desperate need of it?",
+"choices": ["The Room of Requirement", "The Chamber of Secrets", "The Forbidden Forest", "The Astronomy Tower"],
+"answer": "The Room of Requirement"
+},
+
+{
+"question": "What is the name of the wizarding school in France?",
+"choices": ["Beauxbatons", "Durmstrang", "Ilvermorny", "Castelobruxo"],
+"answer": "Beauxbatons"
+},
+
+{
+"question": "What is the name of the game played on broomsticks that involves trying to score points with a ball?",
+"choices": ["Quidditch", "Bludger Ball", "Snitch Catch", "Broomstick Dash"],
+"answer": "Quidditch"
+},
+
+{
+"question": "What is the name of the sport invented by Ron Weasley that involves trying to knock over a group of trolls?",
+"choices": ["Wizard's Chess", "Gobstones", "Exploding Snap", "Wizard's Duel"],
+"answer": "Wizard's Chess"
+},
+
+{
+"question": "What is the name of the goblin who helps Harry, Ron, and Hermione break into Gringotts Bank?",
+"choices": ["Griphook", "Bogrod", "Ragnok", "Travers"],
+"answer": "Griphook"
+},
+
+{
+"question": "What is the name of the magical creature that can transform into any animal?",
+"choices": ["A Metamorphmagus", "An Animagus", "A Boggart", "A Hippogriff"],
+"answer": "An Animagus"
+},
+
+{
+"question": "What is the name of the wizarding newspaper that is read by characters in the Harry Potter series?",
+"choices": ["The Daily Prophet", "The Quibbler", "The Daily Bugle", "The Evening Standard"],
+"answer": "The Daily Prophet"
+},
+
+{
+"question": "What is the name of the magical object that allows the user to turn back time?",
+"choices": ["A Time-Turner", "A Time-Twister", "A Time-Teller", "A Chronomancer"],
+"answer": "A Time-Turner"
+},
+
+{
+"question": "What is the name of the magical object that can show the user their deepest desires?",
+"choices": ["The Mirror of Erised", "The Pensieve", "The Remembrall", "The Sneakoscope"],
+"answer": "The Mirror of Erised"
+},
+
+{
+"question": "What is the name of the magical creature that has the ability to track down and locate a person?",
+"choices": ["A House-elf", "A Thestral", "A Niffler", "A Snidget"],
+"answer": "A Thestral"
+},
+
+{
+"question": "What is the name of the prison that is guarded by Dementors and holds the most dangerous criminals in the wizarding world?",
+"choices": ["Azkaban", "Nurmengard", "Durmstrang", "Gringotts"],
+"answer": "Azkaban"
+},
+
+{
+"question": "What is the name of the potion that can restore a person's sanity?",
+"choices": ["Obliviate", "Draught of Living Death", "Polyjuice Potion", "Mandrake Restorative Draught"],
+"answer": "Mandrake Restorative Draught"
+},
+
+{
+"question": "What is the name of the wizard who was the headmaster of Hogwarts before Albus Dumbledore?",
+"choices": ["Armando Dippet", "Phineas Nigellus Black", "Dilys Derwent", "Brutus Scrimgeour"],
+"answer": "Armando Dippet"
+},
+
+{
+"question": "What is the name of the organization founded by Albus Dumbledore to fight against Voldemort?",
+"choices": ["The Order of the Phoenix", "The Death Eaters", "The Ministry of Magic", "The Knights of Walpurgis"],
+"answer": "The Order of the Phoenix"
+},
+
+{
+"question": "What is the name of the ghost who haunts the girls' bathroom on the second floor of Hogwarts?",
+"choices": ["Moaning Myrtle", "Bloody Baron", "The Grey Lady", "Nearly Headless Nick"],
+"answer": "Moaning Myrtle"
+},
+
+{
+"question": "What is the name of the giant spider that lives in the Forbidden Forest?",
+"choices": ["Aragog", "Acromantula", "Norberta", "Grawp"],
+"answer": "Aragog"
+},
+
+{
+"question": "What is the name of the magical plant that can strangle a person?",
+"choices": ["Mimbulus Mimbletonia", "Devil's Snare", "Gillyweed", "Sneezewort"],
+"answer": "Devil's Snare"
+},
+
+{
+"question": "What is the name of the house-elf who serves the Black family?",
+"choices": ["Kreacher", "Dobby", "Winky", "Hokey"],
+"answer": "Kreacher"
+},
+
+{
+"question": "What is the name of the potion that causes the drinker to fall into a deep sleep?",
+"choices": ["Draught of Living Death", "Polyjuice Potion", "Felix Felicis", "Veritaserum"],
+"answer": "Draught of Living Death"
+},
+
+{
+"question": "What is the name of the magical plant that can be used to regrow bones?",
+"choices": ["Mandrake", "Wolfsbane", "Sopophorous Bean", "Gillyweed"],
+"answer": "Mandrake"
+},
+
+{
+"question": "What is the name of the game that Hermione creates to help Harry prepare for the Triwizard Tournament?",
+"choices": ["Gobstones", "Exploding Snap", "Wizard's Chess", "D.A. Training"],
+"answer": "D.A. Training"
+}
+
+]
+
